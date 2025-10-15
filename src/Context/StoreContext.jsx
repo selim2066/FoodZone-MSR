13bf4da1238edad41db64d6685a56c377605d8fb
selim2066 <mdselimreza2066@gmail.com>
2025-10-15 17:32:44 +0600

    count

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,14 +1,48 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
+
 const StoreContextProvider = ({ children }) => {
-  const contextValue = {food_list};
-  console.log(contextValue)
+  const [cart, setCart] = useState({});
+
+//   const addCartItem = (itemId) => {
+//     if (!cart[itemId]) {
+//       setCart((prev) => ({ ...prev, [itemId]: 1 }));
+//     } else {
+//       setCart((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+//     }
+//   };
+
+const addCartItem = (itemId) => {
+  setCart((prev) => ({
+    ...prev,
+    [itemId]: (prev[itemId] || 0) + 1, 
+  }));
+};
+
+  const removeCartItem = (itemId) => {
+    setCart((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const contextValue = {
+    food_list,
+    addCartItem,
+    removeCartItem,
+    cart,
+    setCart,
+  };
+  console.log(contextValue);
+
+  useEffect(()=>{
+    console.log(cart)
+  },[cart])
+
   return (
     <StoreContext.Provider value={contextValue}>
       {children}
     </StoreContext.Provider>
+    
   );
 };
 
--- src/component/Navbar/foodDisplay/FoodDisplay.jsx
+++ src/component/Navbar/foodDisplay/FoodDisplay.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 
 import { StoreContext } from "../../../Context/StoreContext";
+import { assets } from "../../../assets/assets";
 
 const FoodDisplay = () => {
   const { food_list } = useContext(StoreContext);
+  const { addCartItem, removeCartItem, cart, setCart } =
+    useContext(StoreContext);
   return (
     <>
       <h1 className="text-2xl font-bold text-amber-600 w-11/12 mx-auto">
@@ -12,17 +15,47 @@ const FoodDisplay = () => {
       <div className="grid grid-cols-4 mx-auto w-11/12 gap-x-5 gap-y-10">
         {food_list.map((item, index) => {
           return (
-            <div className="space-y-2">
-              <div className="overflow-hidden rounded-xl">
+            <div className="space-y-2 shadow-lg rounded-2xl">
+              <div className="overflow-hidden rounded-xl p-2 relative ">
                 <img
                   src={item.image}
-                  className="transition-transform duration-300 hover:scale-125"
+                  className="transition-transform duration-300 hover:scale-125 rounded-lg"
                   alt=""
                 />
+                <div className="absolute bottom-5 right-5 bg-amber-50 p-2 rounded-3xl">
+                  {!cart[item._id] ? (
+                    <img
+                      className="w-8"
+                      onClick={() => addCartItem(item._id)}
+                      src={assets.add_icon_white}
+                      alt=""
+                    />
+                  ) : (
+                    <div className="flex items-center gap-2 font-semibold">
+                      <img
+                        className="w-7"
+                        onClick={() => removeCartItem(item._id)}
+                        src={assets.remove_icon_red}
+                        alt=""
+                      />
+                      <p>{cart[item._id]}</p>
+                      <img
+                        className="w-7"
+                        onClick={() => addCartItem(item._id)}
+                        src={assets.add_icon_green}
+                        alt=""
+                      />
+                    </div>
+                  )}
+                </div>
               </div>
-              <h1 className="text-xl font-semibold pt-5">{item.name}</h1>
-              <p className="text-lg">{item.description}</p>
-              <p className="text-lg font-bold">${item.price}</p>
+              <div className="pl-4 pb-5 space-y-3">
+                <h1 className="text-xl font-semibold pt-5">{item.name}</h1>
+                <p className="text-lg text-gray-400">{item.description}</p>
+                <p className="text-xl font-bold text-amber-600">
+                  ${item.price}
+                </p>
+              </div>
             </div>
           );
         })}
